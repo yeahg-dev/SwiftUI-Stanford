6a86b83cc2516bfbcbfb0357f7be62c30b8dbb8e
yeahg-dev <yeahg.dev@gmail.com>
2022-02-24 00:23:59 +0900

    feat: Memorize SwiftUI project 생성

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 1 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
