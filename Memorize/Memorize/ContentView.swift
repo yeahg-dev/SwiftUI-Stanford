c32ddf8f0d1f382bf517b9fb890e2f3800615636
yeahg-dev <yeahg.dev@gmail.com>
2022-02-24 13:29:14 +0900

    feat: ZStack 구현

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, world!")
+        })
+        .padding()
+        .foregroundColor(.red)
     }
 }
 
